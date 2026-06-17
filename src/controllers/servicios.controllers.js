4c8e801f213db8daec978c86b77d89060063afda
Emilse Arias <emilsearias@gmail.com>
2026-06-17 11:41:03 -0300

    agregamos el controlador para listar y crear servicios

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -1,11 +1,16 @@
+import Servicio from "../models/servicio.js";
+
 export const prueba = (req, res) => {
   res.json("hasta luego ultima prueba");
 }
 
-export const crearServicio = (req, res) => {
+export const crearServicio = async(req, res) => {
   try{
     //console.log(req.body)
-    res.json("hasta luego");
+    const servicioNuevo = new Servicio(req.body)
+    //aqui quiero guardar en la BD
+    await servicioNuevo.save()
+    res.status(201).json({mensaje: 'El servicio fue creado correctamente'})
 
   }catch(error){
     console.error(error)
@@ -13,3 +18,15 @@ export const crearServicio = (req, res) => {
   }
   
 }
+
+export const listarServicios = async(req, res) => {
+  try{
+    const servicios = await Servicio.find();
+    res.status(200).json(servicios);
+  }catch(error){
+    console.error(error)
+    res.status(500).json({mensaje: 'Ocurrio un error al listar los servicios'})
+  }
+  
+}
+
--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { crearServicio, prueba } from "../controllers/servicios.controllers.js";
+import { crearServicio, listarServicios, prueba } from "../controllers/servicios.controllers.js";
 
 const router = Router()
 //http://localhost:3000/api/servicios/
@@ -9,6 +9,6 @@ const router = Router()
 //delete - borrar algo
 
 router.route('/test').get(prueba)
-router.route('/').post(crearServicio)
+router.route('/').post(crearServicio).get(listarServicios)
 
 export default router

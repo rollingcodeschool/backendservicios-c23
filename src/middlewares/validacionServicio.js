03ef596f1ab1494fc1d8879fff1a79098bc9d45d
Emilse Arias <emilsearias@gmail.com>
2026-06-24 11:19:20 -0300

    agregamos validaciones al resto de las propiedades del servicio en el middleware personalizado

--- src/middlewares/validacionServicio.js
+++ src/middlewares/validacionServicio.js
@@ -6,8 +6,43 @@ const validacionServicio = [
     .notEmpty()
     .withMessage("El nombre del servicio es un dato obligatorio")
     .isString()
-    .withMessage('El nombre de servicio debe ser un string'),
-    resultadoValidacion
+    .withMessage("El nombre de servicio debe ser un string")
+    .isLength({ min: 5, max: 100 })
+    .withMessage("El nombre servicio debe contener entre 5 y 100 caracteres"),
+  body("precio")
+    .notEmpty()
+    .withMessage("El precio es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un valor numerico")
+    .isFloat({ min: 50 })
+    .withMessage("El precio debe ser desde $50"),
+  body("categoria")
+    .notEmpty()
+    .withMessage("La categoria es un dato obligatorio")
+    .isString()
+    .withMessage("La categoria debe ser un string")
+    .isIn(["Desarrollo Web", "Backend & API", "Consultoría"])
+    .withMessage(
+      "La categoria debe ser algunos de los siguientes valores: 'Desarrollo Web', 'Backend & API', 'Consultoría'",
+    ),
+  body("imagen")
+    .notEmpty()
+    .withMessage("La imagen es un dato obligatorio")
+    .isString()
+    .withMessage("La imagen debe ser un string")
+    .matches(/^https:\/\/.+\.(jpg|jpeg|png|webp|avif|svg)$/)
+    .withMessage(
+      "La imagen debe ser una url valida terminada en jpg|jpeg|png|webp|avif|svg",
+    ),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La descripción es un dato obligatorio")
+    .isString()
+    .withMessage("La descripción debe ser un string")
+    .isLength({ min: 10, max: 500 })
+    .withMessage("La descripción debe contener entre 10 y 500 caracteres"),
+
+  resultadoValidacion,
 ];
 
-export default validacionServicio
+export default validacionServicio;

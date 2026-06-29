6b7f81aad4676c6ae90a2bd789ef4294183b6921
Emilse Arias <emilsearias@gmail.com>
2026-06-29 10:39:29 -0300

    agregamos las reglas de validacion para el caso del metodo patch

--- src/middlewares/validacionServicio.js
+++ src/middlewares/validacionServicio.js
@@ -2,38 +2,34 @@ import { body, param } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
 import Servicio from "../models/servicio.js";
 
-export const validacionServicio = [
+const reglasServicio = [
   body("nombreServicio")
-    .notEmpty()
-    .withMessage("El nombre del servicio es un dato obligatorio")
     .isString()
     .withMessage("El nombre de servicio debe ser un string")
     .isLength({ min: 5, max: 100 })
     .withMessage("El nombre servicio debe contener entre 5 y 100 caracteres")
     .custom(async (valor, { req }) => {
       const servicioBuscado = await Servicio.findOne({ nombreServicio: valor });
-      console.log(servicioBuscado)
+      console.log(servicioBuscado);
       //pregunto si no existe el servicio buscado
-      if(!servicioBuscado){
-        return true
+      if (!servicioBuscado) {
+        return true;
       }
       //verificar si estoy editando
-      if(req.params?.id && servicioBuscado._id.toString() === req.params.id){
-        return true
+      if (req.params?.id && servicioBuscado._id.toString() === req.params.id) {
+        return true;
       }
       //si ya existe el nombre del servicio buscando retorno error
-      throw new Error('El nombre del servicio ya existe en la base de datos, debes crear un nombre nuevo')
+      throw new Error(
+        "El nombre del servicio ya existe en la base de datos, debes crear un nombre nuevo",
+      );
     }),
   body("precio")
-    .notEmpty()
-    .withMessage("El precio es un dato obligatorio")
     .isNumeric()
     .withMessage("El precio debe ser un valor numerico")
     .isFloat({ min: 50 })
     .withMessage("El precio debe ser desde $50"),
   body("categoria")
-    .notEmpty()
-    .withMessage("La categoria es un dato obligatorio")
     .isString()
     .withMessage("La categoria debe ser un string")
     .isIn(["Desarrollo Web", "Backend & API", "Consultoría"])
@@ -41,8 +37,6 @@ export const validacionServicio = [
       "La categoria debe ser algunos de los siguientes valores: 'Desarrollo Web', 'Backend & API', 'Consultoría'",
     ),
   body("imagen")
-    .notEmpty()
-    .withMessage("La imagen es un dato obligatorio")
     .isString()
     .withMessage("La imagen debe ser un string")
     .matches(/^https:\/\/.+\.(jpg|jpeg|png|webp|avif|svg)$/)
@@ -50,14 +44,22 @@ export const validacionServicio = [
       "La imagen debe ser una url valida terminada en jpg|jpeg|png|webp|avif|svg",
     ),
   body("descripcion")
-    .notEmpty()
-    .withMessage("La descripción es un dato obligatorio")
     .isString()
     .withMessage("La descripción debe ser un string")
     .isLength({ min: 10, max: 500 })
     .withMessage("La descripción debe contener entre 10 y 500 caracteres"),
-
+];
+//para validar en post y put
+export const validacionServicio = [
+  ...reglasServicio.map((regla) =>
+    regla.notEmpty().withMessage("Este campo es obligatorio"),
+  ),
   resultadoValidacion,
+];
+
+//patch
+export const validacionServicioPatch = [
+  ...reglasServicio.map((regla) => regla.optional({values: 'falsy'})), resultadoValidacion
 ];
 
 export const validacionIDServicio = [

419e58cab31831329ecfba0b4616ff2883edb358
Emilse Arias <emilsearias@gmail.com>
2026-07-13 10:32:20 -0300

    agregamos variables de entorno y configuramos el mailer usando nodemailer

---
+++ src/utils/mailer.js
@@ -0,0 +1,19 @@
+import nodemailer from "nodemailer";
+
+// Create a transporter using SMTP
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+transporter
+  .verify()
+  .then(() => console.info("📬 Servidor de correos listo para usar"))
+  .catch((error) =>
+    console.error("Error al configurar el transportador de mails", error),
+  );

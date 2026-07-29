8dbc66aa0d2f14986e52eb7e11227a416b0e5c89
Emilse Arias <emilsearias@gmail.com>
2026-07-29 11:22:17 -0300

    creamos el modelo de carrito

---
+++ src/models/carrito.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const carritoSchema = new Schema(
+  {
+    usuario: {
+      type: Schema.Types.ObjectId,
+      ref: "usuario",
+      required: true,
+      unique: true,
+    },
+    items: [
+      {
+        servicio: {
+          type: Schema.Types.ObjectId,
+          ref: "servicio",
+          required: true,
+        },
+        cantidad: {
+          type: Number,
+          default: 1,
+          min: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Carrito = mongoose.model('carrito', carritoSchema)
+
+export default Carrito

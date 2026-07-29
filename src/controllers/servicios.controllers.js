fd325d82a88e420a858fa6e19fad24a0a8b09963
Emilse Arias <emilsearias@gmail.com>
2026-07-29 10:59:50 -0300

    organizamos el codigo de servicio.controllers

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -15,30 +15,33 @@ export const crearServicio = async (req, res) => {
 
 export const listarServicios = async (req, res) => {
   try {
-    const {termino, pagina, cantServicios} = req.query
-    const paginaNumero = parseInt(pagina)
-    const limite = parseInt(cantServicios)
-    const salto = (paginaNumero - 1) * limite
+    const { termino, pagina, cantServicios } = req.query;
+    const paginaNumero = parseInt(pagina);
+    const limite = parseInt(cantServicios);
+    const salto = (paginaNumero - 1) * limite;
 
-    const query ={}
+    const query = {};
 
-    if(termino){
-      query.nombreServicio = { $regex: termino, $options: "i"}
+    if (termino) {
+      query.nombreServicio = { $regex: termino, $options: "i" };
     }
     // tratemos de no usar varias consultas sueltas
     // const servicios = await Servicio.find(query).populate('categoria','nombre descripcion')
     // const cantidadTotal = await Servicio.countDocuments(query)
-    
+
     const [servicios, cantidadTotal] = await Promise.all([
-      Servicio.find(query).populate('categoria','nombre descripcion').skip(salto).limit(limite),
-      Servicio.countDocuments(query)
-    ])
+      Servicio.find(query)
+        .populate("categoria", "nombre descripcion")
+        .skip(salto)
+        .limit(limite),
+      Servicio.countDocuments(query),
+    ]);
 
     res.status(200).json({
       servicios,
       cantidadTotal,
       paginaActual: paginaNumero,
-      totalPaginas: Math.ceil(cantidadTotal / limite)
+      totalPaginas: Math.ceil(cantidadTotal / limite),
     });
   } catch (error) {
     console.error(error);
@@ -51,7 +54,10 @@ export const obtenerServicioPorID = async (req, res) => {
   try {
     console.log(req.params.id);
     //deberia validar que el id exista y sea un id de mongodb
-    const servicioBuscado = await Servicio.findById(req.params.id).populate('categoria','nombre descripcion')
+    const servicioBuscado = await Servicio.findById(req.params.id).populate(
+      "categoria",
+      "nombre descripcion",
+    );
     console.log(servicioBuscado);
     if (!servicioBuscado) {
       return res
@@ -70,34 +76,49 @@ export const obtenerServicioPorID = async (req, res) => {
 export const borrarServicioPorID = async (req, res) => {
   try {
     //deberia validar que el id exista y sea un id de mongodb
-    const servicioBorrado = await Servicio.findByIdAndDelete(req.params.id)
+    const servicioBorrado = await Servicio.findByIdAndDelete(req.params.id);
     if (!servicioBorrado) {
       return res
         .status(404)
         .json({ mensaje: "No se encontro un servicio con el id enviado" });
     }
-    res.status(200).json({mensaje: 'El servicio fue eliminado correctamente'});
+    res
+      .status(200)
+      .json({ mensaje: "El servicio fue eliminado correctamente" });
   } catch (error) {
     console.error(error);
     res
       .status(500)
-      .json({ mensaje: "Ocurrio un error al intentar borrar un servicio por id" });
+      .json({
+        mensaje: "Ocurrio un error al intentar borrar un servicio por id",
+      });
   }
 };
 export const editarServicioPorID = async (req, res) => {
   try {
     //deberia validar que el id exista y sea un id de mongodb
-    const servicioActualizado = await Servicio.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    const servicioActualizado = await Servicio.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
     if (!servicioActualizado) {
       return res
         .status(404)
         .json({ mensaje: "No se encontro un servicio con el id enviado" });
     }
-    res.status(200).json({mensaje: 'El servicio fue editado correctamente', servicio: servicioActualizado});
+    res
+      .status(200)
+      .json({
+        mensaje: "El servicio fue editado correctamente",
+        servicio: servicioActualizado,
+      });
   } catch (error) {
     console.error(error);
     res
       .status(500)
-      .json({ mensaje: "Ocurrio un error al intentar editar un servicio por id" });
+      .json({
+        mensaje: "Ocurrio un error al intentar editar un servicio por id",
+      });
   }
 };

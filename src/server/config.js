16989f625d53175aa79bdb49a198726753122d8d
Emilse Arias <emilsearias@gmail.com>
2026-08-10 11:39:27 -0300

    agregamos la configuracion de cors en config.js

--- src/server/config.js
+++ src/server/config.js
@@ -15,7 +15,14 @@ export default class Server {
   }
   // aqui agregar el resto de los metodos del futuro objeto
   middlewares() {
-    this.app.use(cors()); //permitir conexiones remotas
+    this.app.use(cors(
+      {
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        credentials:true,
+        methods: ["GET", "POST","PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+      }
+    )); //permitir conexiones remotas
     this.app.use(express.json()); // permite interpretar los datos que lleguen en la solicitud en formato json
     this.app.use(morgan("dev"));
     this.app.use(cookieParser())
